d9fadb62ce28852f3c42965e51bb6c7ae931c894
rimantascode <r.preiks@gmail.com>
2020-05-28 08:47:59 +0000

    added session and dicionary

--- run.py
+++ run.py
@@ -1,39 +1,44 @@
+
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 
 def add_messages(username, message):
-    """ add messages to messags list"""
-    now = datetime.now().strftime("%H : %M : %S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 
-def get_all_messages():
-    """ Get all the messages and seperate them with a 'br' """
-    return "<br>".join(messages)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    """ Main page with instructions """
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
     return render_template("index.html")
 
 
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome {0} </h1> {1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    """Creates a new messsage and redirects back to the chat page"""
+    """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
-    return redirect("/"+username)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
